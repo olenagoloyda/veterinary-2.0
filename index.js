b2ced0f220afa6cfc01bc4fee2784ea94c1a6fc2
Olena Holoida <103740675+olenagoloyda@users.noreply.github.com>
2022-08-13 22:03:15 +0300

    created project

---
+++ index.js
@@ -0,0 +1,40 @@
+const menuBtn = document.querySelector(".menu_btn");
+const menu = document.querySelector(".menu");
+const homepageContent = document.querySelector(".homepage_content");
+const header = document.querySelector(".header");
+menuBtn.addEventListener("click", showMenu);
+
+function showMenu() {
+    menu.classList.toggle("hide");
+    homepageContent.classList.toggle("hide");
+    menuBtn.classList.toggle("active");
+  }
+
+const galleryBtn = document.querySelector('.gallery_btn'),
+    hidenPhoto = document.querySelectorAll('.hidden_photo'),
+    gallery = document.querySelector('.gallery_container'),
+    popUpContainer = document.querySelector('.pop-up_gallery'),
+    closePopUP = document.querySelector('.pop-up_close'),
+    popUpContent = document.querySelector('.pop-up_img'),
+    galleryPhoto =document.querySelectorAll('.gallery_photo');
+
+
+
+gallery.addEventListener('click', showPopUp);
+galleryBtn.addEventListener('click', showMorePhoto);
+closePopUP.addEventListener('click',closePop);
+
+function showPopUp(e){
+// let newContent = e.target;
+popUpContent.src = e.target.attributes.src.nodeValue;    
+    console.log(popUpContainer);
+popUpContainer.classList.remove('hide');
+}
+
+function closePop(){
+    popUpContainer.classList.add('hide');
+}
+
+function showMorePhoto(){
+hidenPhoto.forEach(photo => photo.classList.toggle('hidden_photo'));
+}
