cb48c91381b7c59ead40195f26df7849c0bf336e
Olena Holoida <103740675+olenagoloyda@users.noreply.github.com>
2022-08-17 18:31:07 +0300

    added services

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ const menuBtn = document.querySelector(".menu_btn");
 const menu = document.querySelector(".menu");
 const homepageContent = document.querySelector(".homepage_content_container");
 const header = document.querySelector(".logo_header");
-menuBtn.addEventListener("click", showMenu);
+const servicesMenuBtn = document.querySelector('.services_menu_content');
+const servicesContent = document.querySelector('.services_content_container');
+servicesMenuBtn.addEventListener('click', removeContent);
+const servicesContentList = document.querySelectorAll('.services_content');
 
+console.log(servicesContentList);
+function removeContent(e){
+    servicesContentList.forEach(item=>item.classList.remove('active'));
+    showServicesContent(e);
+}
+
+function showServicesContent(e){
+    let btnId = e.target.id;
+    let sectionName = btnId.slice(0,-5);
+let actualContent = document.querySelector(`#${sectionName}_content`);
+actualContent.classList.add('active');
+
+
+
+    console.log(actualContent);
+}
+
+
+
+menuBtn.addEventListener("click", showMenu);
 
 
 function showMenu() {
@@ -30,7 +53,6 @@ galleryBtn.addEventListener('click', showMorePhoto);
 closePopUP.addEventListener('click',closePop);
 
 function showPopUp(e){
-// let newContent = e.target;
 popUpContent.src = e.target.attributes.src.nodeValue;    
     console.log(popUpContainer);
 popUpContainer.classList.remove('hide');
